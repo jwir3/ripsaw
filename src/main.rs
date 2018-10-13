61c70bceefe8334fce3e98aae1bae470d3298b9a
Scott Johnson <jaywir3@gmail.com>
2018-10-13 12:35:29 -0500

    Add a configuration to ripsaw.
    We can add different lumber types, different lengths of lumber, and other
    configurable items in the future. Right now, it only contains the width of
    the blade/cut.
    
    Refs #2.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
+extern crate config;
 extern crate ripsaw;
 
 use std::io;
+use std::collections::HashMap;
 
 use ripsaw::Lumber;
 use ripsaw::CutList;
+use ripsaw::Settings;
 
 fn main() {
+    let mut settings = config::Config::default();
+    let mut settings_hashmap: HashMap<String, String> = HashMap::new();
+    settings
+        .merge(config::File::with_name("ripsaw-config"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("RIPSAW"))
+        .unwrap();
+
+    // Deserialize to a hash map.
+    settings_hashmap = settings.try_into::<HashMap<String, String>>().unwrap();
+    let settings = Settings::new_from_hashmap(settings_hashmap);
+
     let mut input;
-    let mut cut_list = CutList::new(None);
+    let mut cut_list = CutList::new_with_settings(settings);
 
     loop {
         input = String::new();
